167138966ee3796688ff602cb376b95eb7db8042
trsaints <thiago.dev2k3@gmail.com>
2024-12-07 11:08:59 -0300

    feat: create repository pattern for dealing with data persistance

---
+++ ScreenSound/Repositories/Interfaces/IRepository.cs
@@ -0,0 +1,19 @@
+namespace ScreenSound.Repositories.Interfaces;
+
+
+public interface IRepository<T>
+{
+	public Task<bool> Add(T entity);
+
+	public List<T> GetAll();
+
+	public T GetById(ulong? id);
+
+	public T GetByName(string? name);
+
+	public Task<bool> Update(T entity);
+
+	public Task<bool> Delete(ulong? id);
+
+	public abstract bool Exists(T entity);
+}
---
+++ ScreenSound/Repositories/Repository.cs
@@ -0,0 +1,102 @@
+using System.Text.Json;
+using ScreenSound.Models;
+using ScreenSound.Repositories.Interfaces;
+using ScreenSound.Services;
+
+
+namespace ScreenSound.Repositories;
+
+
+public abstract class Repository<T> : IRepository<T> where T : Entity, IReviewable
+{
+	protected          List<T>?          Dataset;
+	protected readonly StorageService<T> StorageService = new();
+
+	protected Repository()
+	{
+		if (Dataset is not null)
+			return;
+
+		Task.Run(async () =>
+		    {
+			    try
+			    {
+				    using var savedData = StorageService.LoadAsync();
+
+				    Dataset = await savedData;
+			    }
+			    catch (Exception e) when (e is JsonException
+			                                   or AggregateException
+			                                   or IOException)
+			    {
+				    Dataset = new List<T>();
+			    }
+		    })
+		    .Wait();
+	}
+
+	public virtual async Task<bool> Add(T entity)
+	{
+		if (Exists(entity)) return false;
+
+		Dataset.Add(entity);
+
+		await StorageService.SaveAsync(Dataset);
+
+		return true;
+	}
+
+	public List<T> GetAll()
+	{
+		return Dataset;
+	}
+
+	public T GetById(ulong? id)
+	{
+		return Dataset.FirstOrDefault(t => t.Id == id);
+	}
+
+	public T GetByName(string? name)
+	{
+		return Dataset.FirstOrDefault(t =>
+		{
+			var nameProperty = t.GetType().GetProperty("Nome");
+
+			var nameValue = nameProperty.GetValue(t).ToString();
+
+			return nameValue is not null && nameValue == name;
+		});
+	}
+
+	public async Task<bool> Update(T entity)
+	{
+		var previousModel = GetById(entity.Id);
+
+		if (previousModel is null) return await Add(entity);
+
+		var removeSuccessful = await Delete(entity.Id);
+
+		if (!removeSuccessful) return false;
+
+		await Add(entity);
+		await StorageService.SaveAsync(Dataset);
+
+		return true;
+	}
+
+	public async Task<bool> Delete(ulong? id)
+	{
+		var modelo = GetById(id);
+
+		if (modelo is null)
+			return false;
+
+		Dataset.Remove(modelo);
+
+		await StorageService.SaveAsync(Dataset);
+
+		return true;
+	}
+
+	public abstract bool Exists(T entity);
+}
