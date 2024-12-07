a2288d87fed2cab46cd5d58c9d3b8028498791a2
trsaints <thiago.dev2k3@gmail.com>
2024-12-07 11:10:30 -0300

    refactor: replace defaults with new layers features

--- ScreenSound/Program.cs
+++ ScreenSound/Program.cs
@@ -1,69 +1,38 @@
-﻿using ScreenSound.Menus;
+﻿using ScreenSound.Contexts.Interfaces;
 using ScreenSound.Models;
+using ScreenSound.Views;
 
-Artist ira = new("Ira!");
-ira.AddReview(new Review(10));
-ira.AddReview(new Review(8));
-ira.AddReview(new Review(6));
-Artist beatles = new("The Beatles");
+Dictionary<int, IContext<IReviewable>> mainOptions       = new();
 
-Dictionary<string, Artist> registeredArtists = new();
-registeredArtists.Add(ira.Name, ira);
-registeredArtists.Add(beatles.Name, beatles);
+Init();
 
-Dictionary<int, Menu> mainOptions = new();
-mainOptions.Add(1, new RegisterArtistMenu());
-mainOptions.Add(2, new RegisterAlbumMenu());
-mainOptions.Add(3, new DisplayArtistsMenu());
-mainOptions.Add(4, new ReviewArtistMenu());
-mainOptions.Add(5, new ReviewAlbumnMenu());
-mainOptions.Add(6, new DisplayDetailsMenu());
-mainOptions.Add(-1, new ExitMenu());
+return;
 
-void DisplayLogo()
-{
-	Console.WriteLine(@"
-
-░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
-██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
-╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
-░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
-██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
-╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░
-");
-	Console.WriteLine("Welcome to Screen Sound 2.0!");
-}
-
-void DisplayOptions()
+void Init()
 {
 	while (true)
 	{
-		DisplayLogo();
-		Console.WriteLine("\n[1] Register an Artist");
-		Console.WriteLine("[2] Register an Album");
-		Console.WriteLine("[3] Display all artists");
-		Console.WriteLine("[4] Review an Artist");
-		Console.WriteLine("[5] Review an Album");
-		Console.WriteLine("[6] View Artist details");
-		Console.WriteLine("[-1] Exit");
-
-		Console.Write("\nYour option (digits only): ");
-
-		var chosenOption = Console.ReadLine()!;
-		var parsedOption = int.Parse(chosenOption);
-
-		if (mainOptions.ContainsKey(parsedOption))
+		string[] options =
 		{
-			var chosenMenu = mainOptions[parsedOption];
-			chosenMenu.Execute(registeredArtists);
+			"Register an Artist", "Register an Album", "Display all artists",
+			"Review an Artist", "Review an Album", "View Artist details", "Exit"
+		};
 
-			if (parsedOption > 0) continue;
+		MenuView mainMenu = new("Main Menu",
+		                        "Welcome to ScreenSound! What would you like to do? ",
+		                        options);
+
+		mainMenu.BuildLayout();
+		mainMenu.ReadEntry();
+
+		if (mainOptions.ContainsKey(mainMenu.ChosenOption))
+		{
+			var chosenContext = mainOptions[mainMenu.ChosenOption];
+			chosenContext.Run();
+
+			if (mainMenu.ChosenOption > 0) continue;
 		}
-
-		if (parsedOption != -1) Console.WriteLine("Invalid Option. Exiting...");
 
 		break;
 	}
 }
-
-DisplayOptions();
