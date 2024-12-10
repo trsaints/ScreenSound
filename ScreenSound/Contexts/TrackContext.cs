4d789271f3d40ec46f0736e3080e1530b2224245
trsaints <thiago.dev2k3@gmail.com>
2024-12-09 23:19:00 -0300

    feat: create a user flow for registering Track data

--- ScreenSound/Contexts/TrackContext.cs
+++ ScreenSound/Contexts/TrackContext.cs
@@ -1,5 +1,6 @@
 using ScreenSound.Models;
 using ScreenSound.Repositories.Interfaces;
+using ScreenSound.Views;
 
 
 namespace ScreenSound.Contexts;
@@ -7,37 +8,78 @@ namespace ScreenSound.Contexts;
 
 public class TrackContext : Context<Track>
 {
-	public TrackContext(IRepository<Track> repository) : base(repository)
+	public TrackContext(IRepository<Track> repository,
+	                    IRepository<Artist> artists,
+	                    IRepository<Album> albums) : base(repository)
 	{
+		_artists = artists;
+		_albums  = albums;
 	}
 
-	public override Task Register()
+	private readonly IRepository<Artist> _artists;
+	private readonly IRepository<Album>  _albums;
+
+	public override async Task Register()
 	{
-		throw new NotImplementedException();
+		InputView userInput = new("Register Track");
+		userInput.BuildLayout();
+
+		userInput.ReadInput("Name", "Track name: ");
+		userInput.ReadInput("Artist", "Artist name: ");
+		userInput.ReadInput("Album", "Album name: ");
+		userInput.ReadInput("Duration", "Duration (seconds): ");
+		userInput.ReadInput("Available", "Available (true/false): ");
+
+		var trackArtist = _artists.GetByName(userInput.GetEntry("Artist"));
+		var trackAlbum  = _albums.GetByName(userInput.GetEntry("Album"));
+
+		if (trackArtist == null)
+		{
+			userInput.ReadInput("Error",
+			                    "Artist not found. Press [Enter] to return.");
+
+			return;
+		}
+
+		if (trackAlbum == null)
+		{
+			userInput.ReadInput("Error",
+			                    "Album not found. Press [Enter] to return.");
+
+			return;
+		}
+
+
+		Track newTrack = new()
+		{
+			Name      = userInput.GetEntry("Name"),
+			ArtistId  = trackArtist.Id,
+			AlbumId   = trackAlbum.Id,
+			Duration  = uint.Parse(userInput.GetEntry("Duration")),
+			Available = bool.Parse(userInput.GetEntry("Available"))
+		};
+
+		var success = await Repository.Add(newTrack);
+
+		if (success)
+		{
+			userInput.ReadInput("Success",
+			                    "Track registered successfully. Press [Enter] to return.");
+
+			return;
+		}
+
+		userInput.ReadInput("Error",
+		                    "Track could not be registered. Press [Enter] to return.");
 	}
 
-	public override void ViewAll()
-	{
-		throw new NotImplementedException();
-	}
+	public override void ViewAll() { throw new NotImplementedException(); }
 
-	public override void ViewDetails()
-	{
-		throw new NotImplementedException();
-	}
+	public override void ViewDetails() { throw new NotImplementedException(); }
 
-	public override Task Remove()
-	{
-		throw new NotImplementedException();
-	}
+	public override Task Remove() { throw new NotImplementedException(); }
 
-	public override Task AddReview()
-	{
-		throw new NotImplementedException();
-	}
+	public override Task AddReview() { throw new NotImplementedException(); }
 
-	public override Task Update()
-	{
-		throw new NotImplementedException();
-	}
+	public override Task Update() { throw new NotImplementedException(); }
 }
