3b9693e0bdc9edf27dd2b9f89ff81b9a8f3e2fbe
trsaints <thiago.dev2k3@gmail.com>
2024-12-10 07:40:32 -0300

    fix: set getter for primary key for enabling property serialization

--- ScreenSound/Models/Entity.cs
+++ ScreenSound/Models/Entity.cs
@@ -3,18 +3,18 @@ namespace ScreenSound.Models;
 
 public abstract class Entity : IReviewable
 {
-	public static ulong NextId;
-
-	private readonly Random _generator = new();
-
-	public readonly ulong Id;
-
 	protected Entity()
 	{
 		Id = NextId;
 
 		_ = ulong.TryParse(_generator.NextInt64().ToString(), out NextId);
 	}
+
+	public static ulong NextId;
+
+	private readonly Random _generator = new();
+
+	public ulong Id { get; }
 
 	public abstract double AverageScore { get; }
 
