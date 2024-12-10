c255ea88814537b1b8f81f4ba79ce516fda9ede6
trsaints <thiago.dev2k3@gmail.com>
2024-12-10 07:47:39 -0300

    remove: remove out-of-scope method

--- ScreenSound/Models/Album.cs
+++ ScreenSound/Models/Album.cs
@@ -34,28 +34,6 @@ public class Album : Entity
 
 	public override void AddReview(Review review) { _scores.Add(review); }
 
-	public void DisplayAlbumTracks(ArtistRepository artists,
-	                               TrackRepository tracks)
-	{
-		var artist = artists.GetById(ArtistId);
-		Console.WriteLine($"Album: \"{Name}\" by \"{artist?.Name}\":\n");
-		StringBuilder trackContent = new();
-
-		foreach (var track in TracksIds)
-		{
-			var trackData = tracks.GetById(track);
-
-			if (trackData is null) continue;
-
-			trackContent.AppendLine(
-				$"Track: \"{trackData.Name}\" \t\t Duration: {trackData.Duration}");
-		}
-
-		Console.WriteLine(trackContent.ToString());
-		Console.WriteLine(
-			$"\nFor listening to the whole album, it takes {GetAlbumDuration(tracks)}s");
-	}
-
 	public uint GetAlbumDuration(TrackRepository repository)
 	{
 		uint duration = 0;
