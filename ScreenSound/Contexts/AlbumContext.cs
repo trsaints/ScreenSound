38843c37e4f65f0b88ffe79f5bb9c8761b5e841c
trsaints <thiago.dev2k3@gmail.com>
2024-12-09 23:18:44 -0300

    feat: create a user flow for registering Album data

--- ScreenSound/Contexts/AlbumContext.cs
+++ ScreenSound/Contexts/AlbumContext.cs
@@ -1,5 +1,6 @@
 using ScreenSound.Models;
 using ScreenSound.Repositories.Interfaces;
+using ScreenSound.Views;
 
 
 namespace ScreenSound.Contexts;
@@ -7,37 +8,56 @@ namespace ScreenSound.Contexts;
 
 public class AlbumContext : Context<Album>
 {
-	public AlbumContext(IRepository<Album> repository) : base(repository)
+	public AlbumContext(IRepository<Album> repository, IRepository<Artist> artists) : base(repository)
 	{
+		_artists = artists;
 	}
 
-	public override Task Register()
+	private readonly IRepository<Artist> _artists;
+
+	public override async Task Register()
 	{
-		throw new NotImplementedException();
+		InputView userInput = new("Register Album");
+		userInput.BuildLayout();
+		userInput.ReadInput("Name", "Album Name: ");
+		userInput.ReadInput("Artist", "Artist Name: ");
+
+		var albumArtist = _artists.GetByName(userInput.GetEntry("Artist"));
+
+		if (albumArtist is null)
+		{
+			userInput.ReadInput("Error",
+			                    "Artist not found. Please register the artist first.");
+
+			return;
+		}
+
+		Album newAlbum = new()
+		{
+			Name     = userInput.GetEntry("Name"),
+			ArtistId = albumArtist.Id
+		};
+
+		var success = await Repository.Add(newAlbum);
+
+		if (success)
+		{
+			userInput.ReadInput("Success",
+			                    "Album registered successfully. Press [Enter] to continue");
+			return;
+		}
+
+		userInput.ReadInput("Error",
+		                    "Album registration failed. Press [Enter] to continue");
 	}
 
-	public override void ViewAll()
-	{
-		throw new NotImplementedException();
-	}
+	public override void ViewAll() { throw new NotImplementedException(); }
 
-	public override void ViewDetails()
-	{
-		throw new NotImplementedException();
-	}
+	public override void ViewDetails() { throw new NotImplementedException(); }
 
-	public override Task Remove()
-	{
-		throw new NotImplementedException();
-	}
+	public override Task Remove() { throw new NotImplementedException(); }
 
-	public override Task AddReview()
-	{
-		throw new NotImplementedException();
-	}
+	public override Task AddReview() { throw new NotImplementedException(); }
 
-	public override Task Update()
-	{
-		throw new NotImplementedException();
-	}
+	public override Task Update() { throw new NotImplementedException(); }
 }
